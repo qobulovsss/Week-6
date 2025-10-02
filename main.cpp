6ccf2f707ec0fb361bd48885f0ae27b32b7f1c4b
qobulovsss <250144@newuu.uz>
2025-10-02 12:59:05 +0500

    changed

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// //problem 1
+// float product (float x, float y) {
+//     return x * y;
+// }
+// int main() {
+//     float x,y;
+//     cout << "Please enter the first number: ";
+//     cin >> x;
+//     cout << "Please enter the second number: ";
+//     cin >> y;
+//     cout << product (x,y) << endl;
+//     cout << "rounded: " << round(product (x,y)) << endl;
+// }
+
+// //problem 2
+// void linearaccelaration(double v1, double v2, double t) {
+//     if (t == 0) {
+//         cout << "Error: Time is not zero!" << endl;
+//     } else {
+//          cout << "Acceleration: " << (v1-v2)/t << endl;
+//      }
+// }
+// int main() {
+//     double v1, v2, t;
+//     cin >> v1 >> v2 >> t;
+//     linearaccelaration(v1, v2, t);
+//     return 0;
+// }
+
+// //problem 3
+// void fun() {
+//     double radius;
+//     cout << "Enter the radius of the circle: ";
+//     cin >> radius;
+//     double circumference = 2 * M_PI * radius;
+//     double area = M_PI * radius * radius;
+//     cout << "The area of the circle is: " << area << endl;
+//     cout << "The circumference is: " << circumference << endl;
+// }
+// int main() {
+//  cout << "Circle calculations: "<< endl;
+//  fun();
+//     return 0;
+// }
+
+//problem 4
