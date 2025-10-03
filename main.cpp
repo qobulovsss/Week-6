2bda78c2bcb080e97dc40adb2a0093ff9a1fb51a
qobulovsss <250144@newuu.uz>
2025-10-03 22:15:45 +0500

    1

--- main.cpp
+++ main.cpp
@@ -47,4 +47,65 @@ using namespace std;
 //     return 0;
 // }
 
-//problem 4
+////problem 4
+//void fun() {
+//double a;
+//double b;
+//cout << "Enter the first parameter: ";
+//cin >> a;
+//cout << "Enter the second parameter: ";
+//cin >> b;
+//double perimeter = (a+b)*2;
+//cout << "The perimeter is " << perimeter << endl;
+//double area = (a+b)*(a+b)*2;
+//cout << "The area is " << area << endl;
+//}
+//int main() {
+//cout << "Rectangle calculations: " << endl;
+//fun();
+//return 0;
+//}
+
+////problem 5
+//void swapIntegers(int& a, int& b) {
+    //a = a ^ b;
+    //b = a ^ b;
+    //a = a ^ b;
+//}
+//int main() {
+    //int x, y;
+    //cout << "Enter the first number: ";
+    //cin >> x;
+    //cout << "Enter the second number: ";
+    //cin >> y;
+//cout << "Before swap: x = " << x << ", y = " << y << endl;
+    //swapIntegers(x, y);
+//cout << "After swap: x = " << x << ", y = " << y << endl;
+//return 0;
+//}
+
+//problem 6
+void Factorial(int n = 1) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+int main() {
+    int num;
+    char choice;
+cout << "Do you want to enter a number? (y/n): ";
+    cin >> choice;
+if (choice == 'y') {
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Factorial of " << num << " = " << Factorial(num) << endl;
+} else {
+    cout << "No parameter. Factorial of 1 " << Factorial() << endl;
+}
+cout << "/nAlternative demonstration: " << endl;
+cout << "Factorial(5) = " << Factorial(5) << endl;
+cout << "Factorial() = " << Factorial() << endl;
+    return 0;
+}
